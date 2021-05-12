a30110ec3ea639e8c6c30639188d5efc5b3a19a6
markgardie <markgardie@users.noreply.github.com>
2021-05-12 18:14:11 +0300

    add chromedriver

--- lab2/src/test/java/FirstClass.java
+++ lab2/src/test/java/FirstClass.java
@@ -5,7 +5,11 @@ public class FirstClass {
 
     @Test
     public void firstTest(){
+        System.setProperty("webdriver.chrome.driver", "E:\\Programming\\KPI_Projects\\QA\\lab2\\src\\main\\resources\\drivers\\chromedriver.exe");
+
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://ccte.nau.edu.ua/");
+
+        driver.quit();
     }
 }
