07d5151a7e96261ca1ebcc37c745212a5b29f699
markgardie <markgardie@users.noreply.github.com>
2021-05-13 18:48:39 +0300

    Create HomePage.java

---
+++ lab2/src/test/java/HomePage.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage  {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public HomePage (WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    public void open(){
+        driver.get("https://rozetka.com.ua/");
+    }
+
+    
+
+}
