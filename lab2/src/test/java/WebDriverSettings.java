9f18f0bbe13cd6aea3668d8519d727a897584af0
markgardie <markgardie@users.noreply.github.com>
2021-05-13 16:26:01 +0300

    Create WebDriverSettings.java

---
+++ lab2/src/test/java/WebDriverSettings.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Set;
+
+public class WebDriverSettings {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
